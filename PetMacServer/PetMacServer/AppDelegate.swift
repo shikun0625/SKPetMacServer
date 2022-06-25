5168638f127554738b6bd5569c0ac7fb8d206525
shikun0625 <johnny0625@yahoo.com>
2022-06-25 21:50:34 +0900

    create project

---
+++ PetMacServer/PetMacServer/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  PetMacServer
+//
+//  Created by 强尼 on 2022/06/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ PetMacServer/PetMacServer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PetMacServer
+//
+//  Created by 强尼 on 2022/06/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
